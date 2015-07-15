74242d43738e7994b0f34ae5787a871218fa6a85
asentis <amysentis@gmail.com>
2015-07-15 14:04:05 -0700

    medication eligibility check added, structure reformatted

--- osf_test.py
+++ osf_test.py
@@ -3,8 +3,32 @@
 import sys
 import csv
 import datetime
+import re
 
-# example usage ./osf_test.py
+# example usage python ./osf_test.py data_file medications_file
+
+
+def main():
+    params = []
+
+    for arg in sys.argv[1:]:
+        params.append(arg)
+    print('Parameters:', params)
+
+    if params != []:
+        filename = params[0]
+        medications = params[1]
+        print(filename, medications)
+
+        recordList = read_file(filename)
+        medDict = read_meds(medications)
+
+        check_eligibility(recordList, medDict)
+
+    else:
+        #filename = ''
+        print('No file specified')
+
 
 def read_file(filename):
     """
@@ -18,24 +42,49 @@ def read_file(filename):
     for record in csv_reader:
         print('record:', record)
         tempList.append(record)
-    print('tempList:', tempList)
+    #print('tempList:', tempList)
     f.close()
+
     return tempList
 
 
-def check_eligibility(records):
+def read_meds(medications):
+
+    print('read_file module:', medications)
+    f = open(medications, 'r')
+    csv_reader = csv.reader(f, delimiter=',')
+    medDict = {}
+    for key, value in csv_reader:
+        medDict[key] = value
+    print('medDict:', medDict)
+    f.close()
+
+    return medDict
+
+
+def check_eligibility(records, meds):
 
     #record_test = records[0]
+    tracking = {}
+    tracking['ineligible'] = 0
+    tracking['eligible'] = 0
+
     for record in records:
-        if p01(record) == 0:
+        #if p01(record) == 0:
+        ineligible = p01(record, meds)
+        if ineligible != []:
+            tracking['ineligible'] += 1
+            print(ineligible)
             print("record", record[0], "- ineligible")
         else:
+            tracking['eligible'] += 1
             print("record", record[0], "- looks eligible -> phone screen")
+    print(tracking)
 
+def p01(record, meds):
 
-def p01(record):
-
-    eligible = 1
+   # eligible = 1
+    ineligible = []
 
     dob = datetime.datetime.strptime(record[6], "%m/%d/%Y").date()
     #print(dob)
@@ -45,17 +94,24 @@ def p01(record):
     #print("=", today.year, "-", dob.year, "-", ((today.month, today.day) < (dob.month, dob.day)))
     #print(age)
 
-    if (age > 13 and age < 21) or (age > 65): #dob
+    if age > 13 and age < 21:
         print("age")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("age < 21")
+    elif age > 65: #dob
+        print("age")
+        #eligible = 0
+        ineligible.append("age > 65")
 
     if record[26] == '2': #handedness (not currently excluding ambidextrous)
-        print("handedness")
-        eligible = 0
+        print("left handed")
+        #eligible = 0
+        ineligible.append("left handed")
 
-    if record[27] == '0': #current pain
-        print("current pain")
-        eligible = 0
+    if record[27] == '0': #current pain -- SHOULD WE BE HOLDING THIS RATHER THAN MAKING INELIGIBLE?
+        print("healthy volunteer")
+        #eligible = 0
+        ineligible.append("healthy volunteer")
 
     pain_loc_male = 0
     pain_loc_female = 0
@@ -64,7 +120,7 @@ def p01(record):
             pain_loc_male += int(record[i])
         else:
             pain_loc_female += int(record[i+74])
-        print(i, i+74, pain_loc_male, pain_loc_female)
+        #print(i, i+74, pain_loc_male, pain_loc_female)
 
     #if sum of all pain locations are >0
     if (record[7] == '1' and pain_loc_male > 0) or (record[7] == '2' and pain_loc_female > 0):
@@ -75,60 +131,74 @@ def p01(record):
                 #if other checkbox not checked
                 if record[183] =='0':
                     print("if - not low back pain")
-                    eligible = 0
+                    #eligible = 0
+                    ineligible.append("not low back pain")
                 #else if othe checkbox check and "back" not in sentence
                 elif "back" not in record[185]: #doesn't have "back"
                     print("elif - 'back' not listed in other,", record[185])
-                    eligible = 0
-
+                    #eligible = 0
+                    ineligible.append("not low back pain")
 
 
     if record[176] == '1': #pain duration (not currently excluding intermittent)
         print("pain duration")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("pain < 3 months")
 
     if record[178] == '1': #other pain disorder (FM)
         print("FM")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("other pain condition - FM")
 
     if record[180] == '1': #other pain disorder (CRPS)
         print("CRPS")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("other pain condition - CRPS")
 
     if record[181] == '1': #other pain disorder (PP)
         print("PP")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("other pain condition - PP")
 
     if record[182] == '1': #other pain disorder (Migraine)
         print("Migraine")
-        eligible = 0
+        #eligible = 0
+        ineligible.append("other pain condition - migraine")
 
     if record[188] != '':
         if int(record[188]) < 4: #NRS
             print("NRS")
-            eligible = 0
+            #eligible = 0
+            ineligible.append("NRS < 4")
 
-    return eligible
+    if record[190] != '':
+        med_ineligible = check_meds(record, meds)
+        if med_ineligible != []:
+            print("Medications")
+            ineligible.extend(med_ineligible)
 
 
-def main():
-    params = []
+    return ineligible
 
-    for arg in sys.argv[1:]:
-        params.append(arg)
-    print('Parameters:', params)
 
-    if params != []:
-        filename = params[0]
-        print(filename)
+def check_meds(record, meds):
 
-        recordList = read_file(filename)
+    ineligible = []
+    if record[190] != '':
+        #extract medications as a list?
+        med_list = re.split(r'[,.\s]\s*', record[190])
 
-        check_eligibility(recordList)
+        for i in med_list:
+            if i in meds.keys():
+                reason = i + " - " + meds[i]
+                ineligible.append(reason)
+        #print("Medications:", ineligible)
 
-    else:
-        #filename = ''
-        print('No file specified')
+    return ineligible
+
+
+
+
 
 if __name__ == '__main__':
    main()
