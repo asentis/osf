a5d63e17c34c0ec9e7afb8ff0df049efe64fd7dd
asentis <amysentis@gmail.com>
2015-08-18 13:29:00 -0700

    implemented hold feature for left-handed, healthies. implemented pregnancy exclusion, gender ambiguity exclusion, intermittent pain exclusion. implemented other pain diagnosis text box check for 'spine' and 'disc' in addition to 'back'.

--- osf.py
+++ osf.py
@@ -23,17 +23,15 @@ def main():
         medications = params[1]
         
         api_url = 'https://redcap-demo.stanford.edu/api/'
-        api_key = 'B2586BF6214D752F17ADF208244EFAE4'
+        api_key = 'your key here'
         project = Project(api_url, api_key)
         record = export_record(project)
-        #print("finished with record export module")
         #print(record)
         #print("sorted", sorted(record.keys()))
         ineligibilityDict = read_to_dict(reasons)
         medDict = read_to_dict(medications)
 
         newRecord = check_eligibility(record, ineligibilityDict, medDict)
-        #print("new record:", newRecord)
         import_record(newRecord, project)
 
     else:
@@ -42,34 +40,23 @@ def main():
 
 
 def export_record(project):
-
-    print('export_record module:')
-    '''
-    api_url = 'https://redcap-demo.stanford.edu/api/'
-    api_key = 'B2586BF6214D752F17ADF208244EFAE4'
-	
-    project = Project(api_url, api_key)
-    '''
+    
     #print("FORMS:", project.forms)
     forms_subset = ['online_screening_form', 'p01_cam_center_phone_screen']
-    #forms = project.forms[0]
-    #forms_subset = project.export_records(forms=forms)
     record = project.export_records(forms=forms_subset)
-    #record = project.export_records()
-    print("record exported, # of keys:", len(record[0].keys()))
-    #record = []
-    return record[0] # return dict of single record
+    #print("record exported, # of keys:", len(record[0].keys()))
+	
+    return record[-1] # return dict of single record (last record)
 
 
 def read_to_dict(filename):
 
-    print('read_to_dict module:', filename)
     f = open(filename, 'r')
     csv_reader = csv.reader(f, delimiter=',')
     newDict = {}
     for key, value in csv_reader:
         newDict[key] = value
-    print('newDict:', newDict)
+    #print('newDict:', newDict)
     f.close()
 
     return newDict
@@ -77,15 +64,19 @@ def read_to_dict(filename):
 
 def check_eligibility(record, ineligibilityDict, meds):
 
-    ineligible, problem_meds = p01(record, meds)
+    ineligible, hold, problem_meds = p01(record, meds)
     if ineligible != []:
         print(ineligible)
         print("record", record['record_id'], "- ineligible")
-        newRecord = update_phone_screen(record, ineligible, ineligibilityDict, problem_meds)
+        newRecord = update_ps_inelig(record, ineligible, ineligibilityDict, problem_meds)
+    elif hold != []:
+        print(hold)
+        print("record", record['record_id'], "- on hold")
+        newRecord = update_ps_hold(record, hold)
     else:
         print("record", record['record_id'], "- looks eligible -> phone screen")
-        newRecord = record
-    #print("newRecord:", newRecord)
+        #newRecord = record
+        newRecord = update_ps_elig(record)
 
     return newRecord
 
@@ -102,76 +93,52 @@ def p01(record, meds):
         #print("age =", today.year, "-", dob.year, "- ((",today.month,",", today.day,") < (",dob.month,",", dob.day,")))")
         #print("=", today.year, "-", dob.year, "-", ((today.month, today.day) < (dob.month, dob.day)))
         #print(age)
-
         if age > 13 and age < 21:
-            print("age")
-            #eligible = 0
+            #print("age")
             ineligible.append("Age < 21 years old")
-            # call to mark as ineligible function here - update phone screen
-
         elif age > 65: #dob
-            print("age")
-            #eligible = 0
+            #print("age")
             ineligible.append("Age > 65 years old")
-           # call to mark as ineligible function here - update phone screen
+    
+    if record['gender'] == '3' or record['gender'] == '4':
+        #print("Gender")
+        ineligible.append("Gender Ambiguity")
+    else:
+        pain_loc_male = 0
+        pain_loc_female = 0
 
-    if record['handedness'] == '2': #handedness (not currently excluding ambidextrous)
-        print("left handed")
-        #eligible = 0
-        ineligible.append("MRI: Left-handed")
-       # call to mark as ineligible function here - update phone screen
-
-    if record['currentpain'] == '0': #current pain -- SHOULD WE BE HOLDING THIS RATHER THAN MAKING INELIGIBLE?
-        print("healthy volunteer")
-        #eligible = 0
-        ineligible.append("Healthy Control")
-       # call to mark as ineligible function here - update phone screen
-
-    pain_loc_male = 0
-    pain_loc_female = 0
+        for i in range(1,10): 
+            if record['gender'] == '1': #male
+                fkey = 'painlocation_male___f0' + str(i)
+                #print("fkey", fkey)
+                pain_loc_male += int(record[fkey])
+                bkey = 'painlocation_male___b0' + str(i)
+                #print("bkey", bkey)
+                pain_loc_male += int(record[bkey])
+            else:
+                fkey = 'painlocation_female___f0' + str(i)
+                #print("fkey", fkey)
+                pain_loc_female += int(record[fkey])
+                bkey = 'painlocation_female___b0' + str(i)
+                #print("bkey", bkey)
+                pain_loc_female += int(record[bkey])
+        for i in range(10,37): # checks up to 36, there are 37 and 38 for male/female back or front (i.e. some aren't checked)
+            if record['gender'] == '1': #male
+                fkey = 'painlocation_male___f' + str(i)
+                #print("fkey", fkey)
+                pain_loc_male += int(record[fkey])
+                bkey = 'painlocation_male___b' + str(i)
+                #print("bkey", bkey)
+                pain_loc_male += int(record[bkey])
+            else:
+                fkey = 'painlocation_female___f' + str(i)
+                #print("fkey", fkey)
+                pain_loc_female += int(record[fkey])
+                bkey = 'painlocation_female___b' + str(i)
+                #print("bkey", bkey)
+                pain_loc_female += int(record[bkey])
+        #print(pain_loc_male, pain_loc_female)
 	
-	#### BELOW LOGIC DOESN'T WORK ANYMORE 
-    '''
-	for i in range(28,102): #27,99
-        if record['gender'] == '1': #male
-            pain_loc_male += int(record[i])
-        else:
-            pain_loc_female += int(record[i+74])
-        #print(i, i+74, pain_loc_male, pain_loc_female)
-	'''
-	
-	#CHECK NEW LOGIC
-    for i in range(1,10): 
-        if record['gender'] == '1': #male
-            fkey = 'painlocation_male___f0' + str(i)
-            print("fkey", fkey)
-            pain_loc_male += int(record[fkey])
-            bkey = 'painlocation_male___b0' + str(i)
-            print("bkey", bkey)
-            pain_loc_male += int(record[bkey])
-        else:
-            fkey = 'painlocation_female___f0' + str(i)
-            print("fkey", fkey)
-            pain_loc_female += int(record[fkey])
-            bkey = 'painlocation_female___b0' + str(i)
-            print("bkey", bkey)
-            pain_loc_female += int(record[bkey])
-    for i in range(10,37): # checks up to 36, there are 37 and 38 for male/female back or front (i.e. some aren't checked)
-        if record['gender'] == '1': #male
-            fkey = 'painlocation_male___f' + str(i)
-            print("fkey", fkey)
-            pain_loc_male += int(record[fkey])
-            bkey = 'painlocation_male___b' + str(i)
-            print("bkey", bkey)
-            pain_loc_male += int(record[bkey])
-        else:
-            fkey = 'painlocation_female___f' + str(i)
-            print("fkey", fkey)
-            pain_loc_female += int(record[fkey])
-            bkey = 'painlocation_female___b' + str(i)
-            print("bkey", bkey)
-            pain_loc_female += int(record[bkey])
-    print(pain_loc_male, pain_loc_female)
     #if sum of all pain locations are >0
     if (record['gender'] == '1' and pain_loc_male > 0) or (record['gender'] == '2' and pain_loc_female > 0):
         #if lower back not checked
@@ -180,77 +147,74 @@ def p01(record, meds):
             if record['paindisorder___3'] == '0':
                 #if other checkbox not checked
                 if record['paindisorder___7'] =='0':
-                    print("if - not low back pain")
-                    #eligible = 0
+                    #print("if - not low back pain")
                     ineligible.append("Not Low Back Pain")
-                    # call to mark as ineligible function here - update phone screen
                 #else if other checkbox checked and "back" not in sentence
-                elif "back" not in record['paindisorder_other']: #doesn't have "back"
-                    print("elif - 'back' not listed in other,", record[185])
-                    #eligible = 0
-                    ineligible.append("Not Low Back Pain")
-                   # call to mark as ineligible function here - update phone screen
-
-
-    if record['painduration'] == '1': #pain duration (not currently excluding intermittent)
-        print("pain duration < 3 months")
-        #eligible = 0
+                else:
+                    for word in ['back', 'spine', 'disc']:
+                        if word not in record['paindisorder_other']:
+                            print("elif", word)
+                            ineligible.append("Not Low Back Pain")
+ 
+    if record['painduration'] == '1' or record['painduration'] == '5': #pain duration (not currently excluding intermittent)
+        #print("not chronic pain")
         ineligible.append("No Chronic Pain")
-       # call to mark as ineligible function here - update phone screen
     
     if record['paindisorder___1'] == '1': #recent surgery
-        print("recent surgery")
+        #print("recent surgery")
         ineligible.append("Other Pain: Recent Surgery")
 		
     if record['paindisorder___2'] == '1': #other pain disorder (FM)
-        print("FM")
-        #eligible = 0
+        #print("FM")
         ineligible.append("Other Pain Condition: FM")
-       # call to mark as ineligible function here - update phone screen
 
     if record['paindisorder___4'] == '1': #other pain disorder (CRPS)
-        print("CRPS")
-        #eligible = 0
+        #print("CRPS")
         ineligible.append("Other Pain Condition: CRPS")
-       # call to mark as ineligible function here - update phone screen
 
     if record['paindisorder___5'] == '1': #other pain disorder (PP)
-        print("PP")
-        #eligible = 0
+        #print("PP")
         #ineligible.append("Other Pain Condition: PP")
         ineligible.append("Other Pain: Pelvic")
-       # call to mark as ineligible function here - update phone screen --> Other pain: pelvic? (no other pain condition: PP option)
 
     if record['paindisorder___6'] == '1': #other pain disorder (Migraine)
-        print("Migraine")
-        #eligible = 0
+        #print("Migraine")
         ineligible.append("Other Pain Condition: Migraines")
-       # call to mark as ineligible function here - update phone screen
 
     if record['painscore_avg'] != '': #NRS
         nrs = record['painscore_avg']
         if (int(nrs) < 4) and (int(nrs) > 0):
-            print("NRS")
-            #eligible = 0
+            #print("NRS")
             ineligible.append("NRS: " + nrs)
-           # call to mark as ineligible function here - update phone screen
-
+    
+    problem_meds = []
     if record['medication_list'] != '':
         med_ineligible, problem_meds = check_meds(record['medication_list'], meds)
         if med_ineligible != []:
-            print("Medications")
+            #print("Medications")
             ineligible.extend(med_ineligible)
-           # call to mark as ineligible function here - update phone screen
 
+    if record['pregnancy'] == 1: #pregnant/breastfeeding
+        print("pregnant/breastfeeding")
+        ineligible.append("MRI: Currently pregnant or breastfeeding")
 
-    return ineligible, problem_meds
+    hold = []
+
+    if record['handedness'] == '2': #handedness (not currently excluding ambidextrous)
+        #print("left handed")
+        hold.append("Left-Handed")
+
+    if record['currentpain'] == '0': #current pain
+        #print("healthy volunteer")
+        hold.append("Healthy Control")
+    
+	
+    return ineligible, hold, problem_meds
 
 
 def check_meds(record_meds, meds):
 
     ineligible = []
-    #if record[190] != '':
-    #extract medications as a list?
     med_list = re.split(r'[,.\s]\s*', record_meds)
     #print("med list", med_list)
     problem_meds = []
@@ -258,10 +222,7 @@ def check_meds(record_meds, meds):
     for i in med_list:
         #print("listed med", i)
         i = i.lower()
-        #print("lowercase", i)
         if i in meds.keys():
-            #reason = i + " - " + meds[i]
-            #ineligible.append(reason)
             #print("problem med", i)
             ineligible.append(meds[i])
             problem_meds.append(i)
@@ -270,44 +231,62 @@ def check_meds(record_meds, meds):
     return ineligible, problem_meds
 
 
-def update_phone_screen(record, ineligible, ineligibilityDict, problem_meds):
+def update_ps_inelig(record, ineligible, ineligibilityDict, problem_meds):
 
     record['admin_relevantstudies___6'] = 1 # mark admin panel as considered for P01
     record['admin_ra_exclude'] = 0 # mark admin panel as appropriate for research
-    
     record['admin_panel_complete'] = 2 # mark admin panel as complete
 
     #record['cmtps_date'] = str(datetime.datetime.now()) #raises error
     record['cmtps_conductedby'] = 'Automatic screen program'
     record['cmtps_eligstatus'] = 2 # mark phone screen as failure
-    
-	
     record['p01_cam_center_phone_screen_complete'] = 2 # mark phone screen as complete
 
-	#### CHECK NEW LOGIC BELOW
     for reason in ineligible: # update failure reasons
         i = ineligibilityDict[reason]
         key = 'cmtps_noteligreasons___' + i
-        #print("error:", i)
         record[key] = 1
-        # NOTE: not currently including list of medications provided in OSF (could put this in eligibility notes)
 
     if problem_meds != []:
         med_string = ''
         for med in problem_meds:
             med_string += (med + " ")
-        #print("med_string", med_string)
         record['cmtps_eligstatusnotes'] = med_string
 
     return record
 
 
+def update_ps_hold(record, hold):
+
+    record['admin_relevantstudies___6'] = 1 # mark admin panel as considered for P01
+    record['admin_ra_exclude'] = 0 # mark admin panel as appropriate for research
+    record['admin_panel_complete'] = 2 # mark admin panel as complete
+
+    record['cmtps_eligstatus'] = 3 # mark phone screen as hold
+    record['p01_cam_center_phone_screen_complete'] = 2 # mark phone screen as complete
+
+    if "Healthy Control" in hold:
+        record['cmtps_holdreason___1'] = 1 
+    if "Left-Handed" in hold:
+        record['cmtps_holdreason___2'] = 1 
+        
+    return record
+    
+    
+def update_ps_elig(record):
+
+    record['admin_panel_complete'] = 0 # mark admin panel as incomplete
+    record['p01_cam_center_phone_screen_complete'] = 0 # mark phone screen as incomplete
+
+    return record
+    
+
 def import_record(record, project):
-    #forms_subset = ['online_screening_form', 'p01_cam_center_phone_screen']
+
     to_import = []
     to_import.append(record)
     response = project.import_records(to_import)
-    print("response", response)
+    #print("response", response)
     return response
 
 
